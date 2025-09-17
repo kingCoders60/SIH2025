ae4d6f41f74d78e3fe52b0f52e3faa15300f3596
SHIVAM <spworld60@gmail.com>
2025-09-17 15:22:31 +0530

    signup & login fixed.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
