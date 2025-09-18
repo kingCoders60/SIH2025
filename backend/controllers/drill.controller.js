0dc1972512d24db22acdd4384aff76c445289499
SHIVAM <spworld60@gmail.com>
2025-09-18 21:22:39 +0530

    drill err fixed.

--- backend/controllers/drill.controller.js
+++ backend/controllers/drill.controller.js
@@ -26,10 +26,47 @@ export const getUserDrills = async (req, res) => {
   try {
     const { userId } = req.params;
     const drills = await Drill.find({ userId }).sort({ date: -1 });
+    // ✅ Agar koi drills na mili to bhi empty array return ho raha hai.
     res.json(drills);
   } catch (err) {
-    res.status(500).json({
-      message: "Some error has occured at getUserDrills" + err.message,
-    });
+    // ✅ Yeh sabse bada fix hai. Error hone par bhi ek empty array return karein.
+    console.error("Error in getUserDrills:", err);
+    res.status(500).json([]); // Ye front-end ko crash hone se bachayega
   }
 };
+
+// import Drill from "../models/drill.model.js";
+
+// export const createDrill = async (req, res) => {
+//   try {
+//     const { userId, type, date } = req.body;
+//     const drill = new Drill({ userId, type, date });
+//     await drill.save();
+//     res.status(200).json({ message: "Drill Created", drill });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Some error has occured at drill Controller" + error.message,
+//     });
+//   }
+// };
+// export const updateDrillStatus = async (req, res) => {
+//   try {
+//   } catch (err) {
+//     res.status(500).json({
+//       message:
+//         "Some error is occured at upDrillStatus in drill controller!" +
+//         err.message,
+//     });
+//   }
+// };
+// export const getUserDrills = async (req, res) => {
+//   try {
+//     const { userId } = req.params;
+//     const drills = await Drill.find({ userId }).sort({ date: -1 });
+//     res.json(drills);
+//   } catch (err) {
+//     res.status(500).json({
+//       message: "Some error has occured at getUserDrills" + err.message,
+//     });
+//   }
+// };
